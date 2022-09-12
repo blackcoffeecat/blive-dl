3d8a92e5d6833aa98fa0db01c706d47758246bd4
BlackCoffeeCat <allen1995.cngz@gmail.com>
2022-09-12 20:40:03 +0800

    fix

--- index.js
+++ index.js
@@ -10,12 +10,13 @@ const cmd = new Command();
 cmd
   .argument('<roomid>')
   .option('-b --background', 'download in background')
+  .option('-q --quiet')
   .option('-d --dir <string>', 'directory')
   .action(main)
   .parse(process.argv);
 
 async function main(roomId, opt) {
-  const {background = false, dir} = opt || {};
+  const {background = false, quiet = false, dir} = opt || {};
   let cwd = process.cwd();
   if (dir) cwd = resolve(cwd, dir);
 
@@ -24,21 +25,23 @@ async function main(roomId, opt) {
     resolveBodyOnly: true,
     searchParams: {
       room_id: roomId,
-      protocol: '0',
-      format: '0',
-      codec: '0',
+      protocol: '1', // 0: http_stream, 1: http_hls
+      format: '2', // 0: flv, 1: ts, 2: mp4
+      codec: '0', // 0: h264, 1: h265
       qn: '10000',
       platform: 'web',
     }
   }).json();
 
-  const object = res.data.playurl_info.playurl.stream.at(0).format.at(0).codec.at(0);
-  const m3u8 = `${object.url_info.at(-1).host}${object.base_url}${object.url_info.at(-1).extra}`;
+  const object = res.data.playurl_info?.playurl.stream.at(0).format.at(0).codec.at(0);
+  if (!object) throw new Error('live not found.')
+  const { host, extra } = object.url_info.at(-1)
+  const m3u8 = `${host}${object.base_url}${extra}`;
 
   if (background) {
-    spawn('youtube-dl', [m3u8, '-o', filename], {cwd, detached: true, stdio: 'ignore'})
+    spawn('youtube-dl', [m3u8, '-o', filename, '-q'], {cwd, detached: true, stdio: 'ignore'})
       .once('spawn', function () { this.unref(); });
   } else {
-    spawn('youtube-dl', [m3u8, '-o', filename], {cwd, stdio: 'inherit'});
+    spawn('youtube-dl', [m3u8, '-o', filename].concat(quiet ? ['-q']: []), {cwd, stdio: 'inherit'});
   }
 }
